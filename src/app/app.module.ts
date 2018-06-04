5363d2117980151a521e9ddc25359295b7eac303
HebaAlali <heba.alali1@gmail.com>
2018-06-04 21:48:27 +0300

    Coding Admin section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,58 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { AdminPage } from '../pages/admin/admin';
+import { FamilyPage } from '../pages/family/family';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { KidServicesProvider } from '../providers/kid-services/kid-services';
+
+
+export const fireconfig = {
+  apiKey: "AIzaSyDW-y5lSGZ9-e8uAUOkLAGHn3EoCJPqwOI",
+  authDomain: "kids-96218.firebaseapp.com",
+  databaseURL: "https://kids-96218.firebaseio.com",
+  projectId: "kids-96218",
+  storageBucket: "kids-96218.appspot.com",
+  messagingSenderId: "485707391906"
+};
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FamilyPage,
+    AdminPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(fireconfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    FamilyPage,
+    AdminPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    KidServicesProvider
   ]
 })
 export class AppModule {}
